ae5a3c01c453c8bb875b4cf424fb1c21f5f2542a
Alexei Penkin <iva13293376@yandex.ru>
2021-11-04 21:15:30 +0500

    Mesto

--- scripts/script.js
+++ scripts/script.js
@@ -3,35 +3,26 @@ const closeButton = popupElement.querySelector('.popup__close-button');
 const profile = document.querySelector('profile');
 const navButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__heading');
-let profileProfession = document.querySelector('.profile__text');
+let profileJob = document.querySelector('.profile__description');
 const formElement = document.querySelector('.popup__profile-edit-form');
 let nameInput = formElement.querySelector('.popup__form-name');
 let jobInput = formElement.querySelector('.popup__form-job');
 
 
-function togglePopup() {
-    popupElement.classList.toggle('hidden');
-}
-
 function openPopup() {
     popupElement.classList.remove('hidden');
     
-    nameInput.value = profileName.textContent('popup__form-name');
-   
-    jobInput.value = profileName.textContent('popup__form-job');
+    nameInput.value = profileName.textContent
+    
+    jobInput.value = profileJob.textContent
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    // Вставьте новые значения с помощью textContent
-    
-    let jobInput = formElement.querySelector('.popup__form-job');
+       
+    profileName.textContent = nameInput.value
 
-    let nameInput = formElement.querySelector('.popup__form-name');
-    
-    jobInput.value = profileName.textContent('popup__form-job');
-
-    nameInput.value = profileName.textContent('popup__form-name');
+    profileJob.textContent = jobInput.value
 
     closePopup();
 }
@@ -51,7 +42,6 @@ document.addEventListener('keyup', (event) => {
     }
 });
 
-// profile.addEventListener('click', () => { debugger }, true);
 popupElement.classList.toggle('hidden');
 
 
