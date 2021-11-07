bf2de31fc090d80b05e4bd2bc4ca98b2a177bcc9
Alexei Penkin <iva13293376@yandex.ru>
2021-11-07 16:38:33 +0500

    Mesto

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,21 @@ const profile = document.querySelector('profile');
 const navButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__heading');
 let profileJob = document.querySelector('.profile__description');
-const formElement = document.querySelector('.popup__profile-edit-form');
-let nameInput = formElement.querySelector('.popup__form-name');
-let jobInput = formElement.querySelector('.popup__form-job');
+const formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.input-form__name');
+let jobInput = formElement.querySelector('.input-form__job');
 
 
 function openPopup() {
     popupElement.classList.remove('hidden');
-    
     nameInput.value = profileName.textContent
-    
     jobInput.value = profileJob.textContent
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-       
     profileName.textContent = nameInput.value
-
     profileJob.textContent = jobInput.value
-
     closePopup();
 }
 
@@ -33,15 +28,9 @@ function closePopup() {
     popupElement.classList.add('hidden');
 }
 navButton.addEventListener('click', openPopup)
-
 closeButton.addEventListener('click', closePopup)
-
 document.addEventListener('keyup', (event) => {
     if (event.key === "Escape") {
         closePopup();
     }
 });
-
-popupElement.classList.toggle('hidden');
-
-
