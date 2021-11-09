57cf0a6b7dd95784636c7ffa5be519195b69a95a
Alexei Penkin <iva13293376@yandex.ru>
2021-11-09 17:20:31 +0500

    Mesto

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-const popupElement = document.querySelector('.popup_opened');
+const popupElement = document.querySelector('.popup');
 const closeButton = popupElement.querySelector('.popup__close-button');
 const profile = document.querySelector('profile');
 const navButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 const formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.input-form_name');
-let jobInput = formElement.querySelector('.input-form_job');
+let nameInput = formElement.querySelector('[name="input-form_name"]');
+let jobInput = formElement.querySelector('[name="input-form_job"]');
 
 
 function openPopup() {
