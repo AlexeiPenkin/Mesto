690c707b49ca6cfb71454dce3668e6346766722d
Alexei Penkin <iva13293376@yandex.ru>
2022-02-08 21:18:41 +0500

    Mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
-import {Card} from './Card.js'
-
-// const popupCloseButton = document.querySelector(".popup__close-button");
+import Card from './Card.js'
 
 const popupProfileEdit = document.querySelector(".popup.edit-profile");
 const inputName = document.querySelector("[name='name']");
@@ -16,8 +14,9 @@ const inputElement = formElement.querySelector(".form__input");
 const submitButton = formElement.querySelector(".form__submit-button");
 const submitPlaceButton = document.querySelector(".form__submit_type_place");
 
-// const cardList = document.querySelector(".card-list");
-// const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".card-list");
+const cardTemplate = document.querySelector("#card-template");
+const cardListItem = document.querySelector(".card");
 
 const popupAddCard = document.querySelector(".popup.add-card");
 const addCardButton = document.querySelector(".profile__add-button");
@@ -25,47 +24,73 @@ const inputCardName = document.querySelector("[name='title']");
 const inputCardLink = document.querySelector("[name='link']");
 const addCardCloseButton = document.querySelector(".popup.add-card .popup__close-button");
 
-// const popupImageOpen = document.querySelector(".popup.popup-image");
-// const popupImage = document.querySelector(".popup-image__image");
-// const popupImageTitle = document.querySelector(".popup-image__title");
-// const popupImageCloseButton = document.querySelector(".popup.popup-image .popup__close-button");
+const popup = document.querySelector('.popup');
+export const popupImageOpen = document.querySelector(".popup.popup-image");
+export const popupImage = document.querySelector(".popup-image__image");
+export const popupImageTitle = document.querySelector(".popup-image__title");
+const popupImageCloseButton = document.querySelector(".popup.popup-image .popup__close-button");
 
-// Переборр массива - вставка начальных карточек - создание новой карточки //
+const initialCards = [
+  {
+    name: "Архангельская область",
+    link: "https://sun9-12.userapi.com/impf/c851220/v851220531/104efb/ScYk7ynMvXU.jpg?size=1280x874&quality=96&sign=bab021682a9afacd0853d86a143a39ef&type=album",
+  },
+  {
+    name: "Владивосток",
+    link: "https://content.r9cdn.net/rimg/dimg/5a/24/84a52653-city-17546-173323f0b02.jpg?width=1200&height=630&crop=true",
+  },
+  {
+    name: "Красноярский край",
+    link: "https://www.avtodispetcher.ru/wp-content/gallery/krasnoyarsky-krai/2.jpg",
+  },
+  {
+    name: "Казань",
+    link: "https://images.unsplash.com/photo-1628066068625-015ea7bcc21a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cnVzc2lhJTIwc2lnaHRzZWVpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Санкт-Петербург",
+    link: "https://images.unsplash.com/photo-1603955129944-7f2dbff89b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Сочи",
+    link: "https://images.unsplash.com/photo-1576096945991-8f93bc88e924?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1727&q=80",
+  },
+];
 
-// const createCard = (cardInfo) => {
-//   const cardListItem = cardTemplate.content.querySelector(".card").cloneNode(true);
-//   cardListItem.querySelector(".card__name").textContent = cardInfo.name;
-//   cardListItem.querySelector(".card__image").alt = cardInfo.name;
-//   cardListItem.querySelector(".card__image").src = cardInfo.link;
+const createCard = (cardInfo) => {
+  const cardListItem = cardTemplate.content.querySelector(".card").cloneNode(true);
+  cardListItem.querySelector(".card__name").textContent = cardInfo.name;
+  cardListItem.querySelector(".card__image").alt = cardInfo.name;
+  cardListItem.querySelector(".card__image").src = cardInfo.link;
 
-//   cardListItem.querySelector(".card__image").addEventListener("click", () => {
-//     openPopup(popupImageOpen);
-//     popupImageTitle.textContent = cardInfo.name;
-//     popupImage.src = cardInfo.link;
-//     popupImage.alt = ("Фотография" + ": " + cardInfo.name);
-//   });
+  cardListItem.querySelector(".card__image").addEventListener("click", () => {
+    openPopup(popupImageOpen);
+    popupImageTitle.textContent = cardInfo.name;
+    popupImage.src = cardInfo.link;
+    popupImage.alt = ("Фотография" + ": " + cardInfo.name);
+  });
 
-  // const deleteButton = cardListItem.querySelector(".card__button_delete");
-  // deleteButton.addEventListener("click", () => {
-  //   cardListItem.remove();
-  // });
+  const deleteButton = cardListItem.querySelector(".card__button_delete");
+  deleteButton.addEventListener("click", () => {
+    cardListItem.remove();
+  });
 
-//   const likeButton = cardListItem.querySelector(".card__button_like");
-//   likeButton.addEventListener("click", function (evt) {
-//     evt.target.classList.toggle("card__button_like_active");
-//   });
+  const likeButton = cardListItem.querySelector(".card__button_like");
+  likeButton.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("card__button_like_active");
+  });
 
-//   return cardListItem;
-// };
+  return cardListItem;
+};
 
-export const renderCard = (name, link) => {
+const renderCard = (name, link) => {
   const newCard = createCard({ name, link });
 
   cardList.prepend(newCard);
 };
 
-// const cards = initialCards.map(createCard);
-// cardList.prepend(...cards);
+const cards = initialCards.map(createCard);
+cardList.prepend(...cards);
 
 // Функция открытия модального окна (openPopup) //
 export function openPopup(popup) {
@@ -87,8 +112,8 @@ export function closeByEscape(evt) {
   }
 }
 
-// // Функция закрытия модального окна нажатием на Overlay + Close Button //
-export const popups = document.querySelectorAll('.popup');
+// Функция закрытия модального окна нажатием на Overlay + Close Button //
+const popups = document.querySelectorAll('.popup');
 
 popups.forEach((popup) => {
   popup.addEventListener('mousedown', (evt) => {
